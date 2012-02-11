83c91e3bc94fd8b451742e644b754e1729488160
Harrison Houghton <actorclavilis@gmail.com>
2012-02-11 15:21:10 -0500

    Added util.EnemyDeletable that somehow went missing. Signed-off-by: Harrison Houghton <actorclavilis@gmail.com>

---
+++ util/EnemyDeletable.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *
+ * @author harrison
+ */
+public interface EnemyDeletable {
+    public void deleteIf(EnemyPredicate e);
+}
