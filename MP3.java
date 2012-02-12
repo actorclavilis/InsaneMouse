8aa25541fe98b4cab6cbeaebd3b0a1c29e93d640
Harrison Houghton <actorclavilis@gmail.com>
2012-02-11 21:20:40 -0500

    Added audioSetup() Audio might work. Signed-off-by: Harrison Houghton <actorclavilis@gmail.com>

--- MP3.java
+++ MP3.java
@@ -22,9 +22,7 @@ class MP3
 
     public void play() 
     {     
-        Thread t = new Thread() 
-        {
-            @Override
+        Thread t = new Thread() {
             public void run() 
             {
                 while(true)
